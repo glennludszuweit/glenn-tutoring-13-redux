fec01891e315d71decfcf649dfdffbf0a353bf62
glenn <glenn.ludszuweit@gmail.com>
2020-11-13 23:02:10 +0100

    post view condition

--- src/Components/PostView.jsx
+++ src/Components/PostView.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import PostsList from './PostsList';
 
 function PostView() {
   const posts = useSelector((state) => state.postsReducer);
@@ -10,11 +11,13 @@ function PostView() {
   const index = posts.findIndex((post) => post.title === title);
   const post = posts[index];
 
-  return (
+  return post ? (
     <Card className='my-3' style={{ border: 'none' }}>
       <Card.Img src={post.url} />
       <Card.Body as='h1'>{post.title}</Card.Body>
     </Card>
+  ) : (
+    <PostsList />
   );
 }
 
